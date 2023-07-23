b946d801525fda4df6b0d8baab91dd61563ea836
guihendro <ghendro88@gmail.com>
2023-07-24 00:37:31 +0800

    Entity for employee, transcript, recording, recording analysis

---
+++ fyp/src/main/java/com/example/fyp/entity/Employee.java
@@ -0,0 +1,29 @@
+package com.example.fyp.entity;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
+import lombok.Data;
+
+@Data
+@Entity
+public class Employee {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer employeeId;
+
+    private String employeeName;
+    private Integer numCallsHandled;
+    private Integer numPositiveSentiment;
+    private Integer numNegativeSentiment;
+
+    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Recording> recordings;
+
+}
